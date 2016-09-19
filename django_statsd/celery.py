f9b4e9060028a8a15278107efe6b42127d0bf2c7
Jian Yuan Lee <jianyuan@gmail.com>
2016-09-19 18:16:50 +0100

    Log task publish times

--- django_statsd/celery.py
+++ django_statsd/celery.py
@@ -1,20 +1,33 @@
-
-
-from django_statsd.clients import statsd
 import time
 
+from django_statsd.clients import statsd
+
 _task_start_times = {}
+_task_publish_start_times = {}
 
 
-def on_task_sent(sender=None, task_id=None, task=None, **kwds):
+def on_before_task_publish(sender=None, **kwargs):
     """
-    Handle Celery ``task_sent`` signals.
+    Handle Celery ``before_task_publish`` signals.
+    """
+    _task_publish_start_times[sender] = time.time()
+
+
+def on_after_task_publish(sender=None, **kwargs):
+    """
+    Handle Celery ``after_task_publish`` signals.
     """
     # Increase statsd counter.
-    statsd.incr('celery.%s.sent' % task)
+    statsd.incr('celery.%s.sent' % sender)
+
+    # Log publish duration.
+    start_time = _task_publish_start_times.pop(sender, False)
+    if start_time:
+        ms = int((time.time() - start_time) * 1000)
+        statsd.timing('celery.%s.publish.runtime' % sender, ms)
 
 
-def on_task_prerun(sender=None, task_id=None, task=None, **kwds):
+def on_task_prerun(sender=None, task_id=None, task=None, **kwargs):
     """
     Handle Celery ``task_prerun``signals.
     """
@@ -25,7 +38,7 @@ def on_task_prerun(sender=None, task_id=None, task=None, **kwds):
     _task_start_times[task_id] = time.time()
 
 
-def on_task_postrun(sender=None, task_id=None, task=None, **kwds):
+def on_task_postrun(sender=None, task_id=None, task=None, **kwargs):
     """
     Handle Celery ``task_postrun`` signals.
     """
@@ -39,7 +52,7 @@ def on_task_postrun(sender=None, task_id=None, task=None, **kwds):
         statsd.timing('celery.%s.runtime' % task.name, ms)
 
 
-def on_task_failure(sender=None, task_id=None, task=None, **kwds):
+def on_task_failure(sender=None, task_id=None, task=None, **kwargs):
     """
     Handle Celery ``task_failure`` signals.
     """
@@ -53,7 +66,8 @@ def register_celery_events():
     except ImportError:
         pass
     else:
-        signals.task_sent.connect(on_task_sent)
+        signals.before_task_publish.connect(on_before_task_publish)
+        signals.after_task_publish.connect(on_after_task_publish)
         signals.task_prerun.connect(on_task_prerun)
         signals.task_postrun.connect(on_task_postrun)
         signals.task_failure.connect(on_task_failure)
